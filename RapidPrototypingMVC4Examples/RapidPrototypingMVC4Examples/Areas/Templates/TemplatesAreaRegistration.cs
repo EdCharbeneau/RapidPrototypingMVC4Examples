e14a5998b56a17c6b2c4f6c7511a613fe88a1185
Ed Charbeneau <ed.charbeneau@gmail.com>
2012-10-19 16:00:40 -0400

    Added templates
    Needs to be refined and added to the menu.

---
+++ RapidPrototypingMVC4Examples/RapidPrototypingMVC4Examples/Areas/Templates/TemplatesAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace RapidPrototypingMVC4Examples.Areas.Templates
+{
+    public class TemplatesAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Templates";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Templates_default",
+                "Templates/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
