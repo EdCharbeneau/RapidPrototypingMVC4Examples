9cf048f91db0bce2e7dbd873f5cd6ad6befb3937
Ed Charbeneau <ed.charbeneau@gmail.com>
2012-10-20 19:03:17 -0400

    Templates complete

---
+++ RapidPrototypingMVC4Examples/RapidPrototypingMVC4Examples/Controllers/TemplatesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RapidPrototypingMVC4Examples.Controllers
+{
+    public class TemplatesController : Controller
+    {
+        //
+        // GET: /Templates/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BandedTemplate()
+        {
+            return View();
+        }
+
+        public ActionResult BlogTemplate()
+        {
+            return View();
+        }
+
+        public ActionResult ContactTemplate()
+        {
+            return View();
+        }
+
+        public ActionResult FeedTemplate()
+        {
+            return View();
+        }
+
+        public ActionResult GridTemplate()
+        {
+            return View();
+        }
+
+        public ActionResult HomePageTemplate()
+        {
+            return View();
+        }
+        public ActionResult OrbitHomePageTemplate()
+        {
+            return View();
+        }
+        public ActionResult SideBarTemplate()
+        {
+            return View();
+        }
+    }
+}
